f8fbe397ebd19881878cd4773911e49103264028
XzSry <xzzzzzzzz_xz@163.com>
2022-03-28 14:02:32 +0800

    代码初体验

---
+++ src/pages/Blog.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>这个是博客</h1>
+  <router-link to="/">首页</router-link>
+  <router-link to="/blog">博客</router-link>
+  <router-view></router-view>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
